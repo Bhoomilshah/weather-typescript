e8cadc5629cd88023485da005a778eae3741ae45
Bhoomil Shah <eincipientinfo@gmail.com>
2020-05-29 12:46:58 +0530

    console log loaciton's name , current time and weather data.

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,18 @@ const weather = new WeatherService('current')
 async function getWeather(): Promise<void> {
   for (const location of locations) {
     let result: Weather = await weather.getLocationWeather(location)
+    console.log(`---------- Weather Data of ${location} ----------`)
+    console.log(`Name of location: ${result.location.name}`)
+    console.log(`Current time : ${result.location.localtime}`)
+    console.log(`temperature : ${result.current.temperature}`)
+    console.log(`wind_speed  : ${result.current.wind_speed}`)
+    console.log(`wind_degree : ${result.current.wind_degree}`)
+    console.log(`wind_dir : ${result.current.wind_dir}`)
+    console.log(`pressure : ${result.current.pressure}`)
+    console.log(`humidity : ${result.current.humidity}`)
+    console.log(`cloudcover : ${result.current.cloudcover}`)
+    console.log(`uv_index : ${result.current.uv_index}`)
+    console.log(`visibility : ${result.current.visibility}`)
   }
 }
 getWeather()
